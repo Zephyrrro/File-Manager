0bdff0226f89f3728a7a69db13ae9cd4eba19178
Zephyrrro <1103452257@qq.com>
2020-10-09 21:51:14 +0800

    chore: add item click rippe

--- app/src/main/java/com/example/filemanager/Activity/ChildActivity.java
+++ app/src/main/java/com/example/filemanager/Activity/ChildActivity.java
@@ -15,15 +15,18 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.filemanager.R;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ChildActivity extends AppCompatActivity {
   private List<FileView> fileList;
+  private FileViewAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +46,45 @@ public class ChildActivity extends AppCompatActivity {
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
-    if (item.getItemId() == android.R.id.home) {
-      finish();
-      return true;
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    switch (id) {
+      case android.R.id.home:
+        finish();
+        return true;
+      case R.id.sort_by_default:
+        if (!item.isChecked()) {
+          Collections.sort(fileList, GetFilesUtils.getInstance().fileOrder(GetFilesUtils.SORT_BY_DEFAULT));
+          adapter.notifyDataSetChanged();
+          item.setChecked(true);
+        }
+        return true;
+      case R.id.sort_by_size:
+        if (!item.isChecked()) {
+          Collections.sort(fileList, GetFilesUtils.getInstance().fileOrder(GetFilesUtils.SORT_BY_SIZE));
+          adapter.notifyDataSetChanged();
+          item.setChecked(true);
+        }
+        return true;
+      case R.id.sort_by_time:
+        if (!item.isChecked()) {
+          Collections.sort(fileList, GetFilesUtils.getInstance().fileOrder(GetFilesUtils.SORT_BY_TIME));
+          adapter.notifyDataSetChanged();
+          item.setChecked(true);
+        }
+        return true;
     }
     return super.onOptionsItemSelected(item);
   }
@@ -58,7 +95,8 @@ public class ChildActivity extends AppCompatActivity {
     RecyclerView recyclerView = findViewById(R.id.file_container_child);
     LinearLayoutManager layoutManager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(layoutManager);
-    FileViewAdapter adapter = new FileViewAdapter(fileList);
+
+    adapter = new FileViewAdapter(fileList);
     recyclerView.setAdapter(adapter);
     recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
   }
